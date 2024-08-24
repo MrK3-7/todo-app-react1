7431437adf36e32beefa32c513a875ac9ea2810d
MrK3-7 <glavnyak9@gmail.com>
2024-08-24 15:02:17 +0500

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ToDo from "./components/ToDo";
+
+const App = () => {
+  return (
+    <main className="h-screen grid place-items-center">
+      <ToDo />
+    </main>
+  );
+};
+
+export default App;
---
+++ src/components/Box.jsx
@@ -0,0 +1,8 @@
+const Box = ({ children }) => {
+    return(
+        <div className="shadow-lg p-4 rounded-lg bg-slate-500">{ children }</div>
+    )
+    
+}
+
+export default Box;
