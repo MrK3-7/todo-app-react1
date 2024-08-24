57ae17c7265a73af5aeb1105147db1e03b2ec7d4
MrK3-7 <glavnyak9@gmail.com>
2024-08-24 14:45:09 +0500

    Initial setup

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+import ToDo from "./components/ToDo"; 
+const App = () => {
+  return (
+    <main className="h-screen grid place-items-center">
+      <h1>ToDo</h1>
+      <ToDo />
+    </main>
+  );
+};
+
+export default App;
---
+++ vite-project/src/components/Box.jsx
@@ -0,0 +1,7 @@
+const Box = ({ children }) => {
+    return(
+        <div className="shadow-lg p-4 rounded-lg bg-slate-500">{ children }</div>
+    )
+}
+
+export default Box;
